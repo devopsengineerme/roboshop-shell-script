465a37049b1b0dfe623ddee16469ce8135b0dbef
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 18:35:40 +0530

    updating

--- roboshop.sh
+++ roboshop.sh
@@ -4,12 +4,12 @@ Securirygroup=sg-0461e1a264c88f92b
 instance=("web" "cart" "catalogue" "dispatch" "mongodb" "mysql" "rabbitmq" "payment" "redis" "shipping" "user")
 for i in "${instance[@]}"
 do
-echo "instance is $i "
-if [ $i -eq "mysql" ] || [ $i -eq "mongodb" ] || [ $i -eq "shipping" ]
-then
-instance_type="t3.small"
-else
-instance_type="t2.micro"
-fi
-aws ec2-run instances --image-id $AMI --instance-type $instance_type --security-group-ids $securitygroup --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
+    echo "instance is $i "
+    if [ $i == "mysql" ] || [ $i == "mongodb" ] || [ $i == "shipping" ]
+    then
+    instance_type="t3.small"
+    else
+    instance_type="t2.micro"
+    fi
+aws ec2 run-instances --image-id $AMI --instance-type $instance_type --security-group-ids $securitygroup --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
 done
