35ca139778618b7a0671740f118ee43fefd0661c
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 17:09:19 +0530

    updating

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 AMI-ID=$1
 Securiry-group=$2
-instance=(web cart catalogue dispatch mongodb mysql rabbitmq payment redis shipping)
-if [ 'instance{$@}' -eq cart|redis|rabbitmq|payment|web|dispatch|catalogue ]
+instance=(web cart catalogue dispatch mongodb mysql rabbitmq payment redis shipping user)
+for i in "${instance[@]}"
+do
+if [ $i == web ] || [ $i == cart ] || [ $i == catalogue ] || [ $i == dispatch ]
+|| [ $i == rabbitmq ] || [ $i == payment ] || [ $i == redis ] || [ $i == user ]
 then
 instance-type=t2.micro
 else
@@ -10,3 +13,4 @@ instance-type=t3.micro
 fi
 aws ec2-run instances --image-id $AMI-ID --instance-type $instance-type --count 1 --security-group-ids $security-group/
 --key-name $instance
+done
