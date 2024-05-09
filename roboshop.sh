9f79af2c20ce5a8c3df086f77b546b600590a1f1
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 17:16:11 +0530

    updating

--- roboshop.sh
+++ roboshop.sh
@@ -4,8 +4,7 @@ Securirygroup=$2
 instance=(web cart catalogue dispatch mongodb mysql rabbitmq payment redis shipping user)
 for i in "${instance[@]}"
 do
-if [ $i == web ] || [ $i == cart ] || [ $i == catalogue ] || [ $i == dispatch ] /
-|| [ $i == rabbitmq ] || [ $i == payment ] || [ $i == redis ] || [ $i == user ]
+if [ $i == web ] || [ $i == cart ] || [ $i == catalogue ] || [ $i == dispatch ] || [ $i == rabbitmq ] || [ $i == payment ] || [ $i == redis ] || [ $i == user ]
 then
 instance-type=t2.micro
 else
