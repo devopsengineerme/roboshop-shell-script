ed29ed91469badea92ed2d6d20c994685327a663
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 16:17:41 +0530

    updating

---
+++ roboshop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+AMI-ID=$1
+Securiry-group=$2
+instance=(web cart catalogue dispatch mongodb mysql rabbitmq payment redis shipping)
+if [ $instance -eq cart|redis|rabbitmq|payment|web|dispatch|catalogue ]
+then
+instance-type=t2.micro
+else
+instance-type=t3.micro
+fi
+aws ec2-run instances --image-id $AMI-ID --instance-type $instance-type --count 1 --security-id $security-group
