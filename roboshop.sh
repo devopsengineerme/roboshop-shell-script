60d178a4daae291b59471e7ff0a1a350f7f1affc
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 18:30:53 +0530

    updating

--- roboshop.sh
+++ roboshop.sh
@@ -5,11 +5,11 @@ instance=("web" "cart" "catalogue" "dispatch" "mongodb" "mysql" "rabbitmq" "paym
 for i in "${instance[@]}"
 do
 echo "instance is $i "
-if [ $i == "mysql" ] || [ $i == "mongodb" ] || [ $i == "shipping" ]
+if [ $i -eq "mysql" ] || [ $i -eq "mongodb" ] || [ $i -eq "shipping" ]
 then
 instance_type="t3.small"
 else
 instance_type="t2.micro"
 fi
-aws ec2-run instances --image-id ami-0f3c7d07486cad139 --instance-type $instance_type --security-group-ids sg-0461e1a264c88f92b --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
+aws ec2-run instances --image-id $AMI --instance-type $instance_type --security-group-ids $securitygroup --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
 done
