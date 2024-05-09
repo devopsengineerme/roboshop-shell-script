bc163f2a9eddb485648b6658ea0293f723fc6d5d
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-09 17:59:50 +0530

    updating

--- roboshop.sh
+++ roboshop.sh
@@ -4,11 +4,12 @@ Securirygroup=$2
 instance=(web cart catalogue dispatch mongodb mysql rabbitmq payment redis shipping user)
 for i in "${instance[@]}"
 do
+echo "instance is $i "
 if [ $i == web ] || [ $i == cart ] || [ $i == catalogue ] || [ $i == dispatch ] || [ $i == rabbitmq ] || [ $i == payment ] || [ $i == redis ] || [ $i == user ]
 then
-instance-type="t2.micro"
+instance_type="t2.micro"
 else
-instance-type="t3.small"
+instance_type="t3.small"
 fi
-aws ec2-run instances --image-id $AMIID --instance-type $instance-type --count 1 --security-group-ids $securitygroup --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
+aws ec2-run instances --image-id $AMIID --instance-type $instance_type --security-group-ids $securitygroup --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
 done
