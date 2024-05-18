6eb45e0433b642ebdd6bc4fa46e53cdfea468435
devopsengineerme <sirishapasupulety@gmail.com>
2024-05-18 19:56:30 +0530

    check

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 AMI=ami-0f3c7d07486cad139
-Securirygroup=sg-0461e1a264c88f92b
-zoneid=Z0030464251IYYT5PS6JF
+Securirygroup=sg-043763c11599aa7e4
+zoneid=Z0513630MNWLL52V7UP3
 instance=("web" "cart" "catalogue" "dispatch" "mongodb" "mysql" "rabbitmq" "payment" "redis" "shipping" "user")
 for i in "${instance[@]}"
 do
@@ -12,10 +12,12 @@ do
     else
     instance_type="t2.micro"
     fi
-IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $instance_type --security-group-ids $securitygroup \
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $instance_type --security-group-ids $Securitygroup \
  --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value= $i}]"\
  --query 'Instances[0].[PrivateIpAddress]' --output text)
+ aws ec2 create-tags --resources instance --tag Key=Name,Value="$i"
 echo " instance is $i: IP_ADDRESS is $IP_ADDRESS"
+#########################################################################
 ## CREATING ROUTE 53 records
 aws route53 change-resource-record-sets \
   --hosted-zone-id $zoneid \
@@ -25,7 +27,7 @@ aws route53 change-resource-record-sets \
     ,"Changes": [{
       "Action"              : "CREATE"
       ,"ResourceRecordSet"  : {
-        "Name"              : "'$i'.'myfirstroboshop.online'"
+        "Name"              : "'"$i"'.myfirstroboshop.online"
         ,"Type"             : "A"
         ,"TTL"              : 1
         ,"ResourceRecords"  : [{
